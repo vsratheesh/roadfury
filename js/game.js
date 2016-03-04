1c43e16a9222219e68f8f462d8c83abd842c6b3a
vsratheesh <vsratheesh95@gmail.com>
2016-03-04 17:11:59 +0530

    started race

--- js/game.js
+++ js/game.js
@@ -1,13 +1,47 @@
+var Game = {
+	my_pos: 0,// Zero from left
+	enemy_pos: -20, // Position from top
+	init: function () {
+		setInterval(function() {
+			if (Game.enemy_pos > 100) {
+				Game.enemy_pos = -20;
+				var left = Math.floor(Math.random()*5) * 10;
+				Game.enemy.css("left", left + "vmin");
+			} else {
+				Game.enemy_pos += 1;
+				Game.enemy.css("top", Game.enemy_pos + "vmin");
+			}
+			
+		}, 30);
+	},
+	//this || Game 
+	moveRight: function() {
+		if (this.my_pos < 40) {
+			this.my_pos += 10;
+			this.car.css("left", this.my_pos + "vmin");
+		}
+	},
+	moveLeft: function() {
+		if (this.my_pos > 0) {
+			this.my_pos -= 10;
+			this.car.css("left", this.my_pos + "vmin");
+		}
+	}
+}; 
+
+
 $(document).ready(function() {
-  var position = -20;
-  setInterval(function(){
-    var enemy = $('#enemy_car');
-    if (position > 100) {
-      position = -20;
-      var left = Math.floor(Math.random()*5) * 10;
-      enemy.css("left", left + "vmin");
-    }
-    position += 1;
-    enemy.css("top", position + "vmin");
-  }, 30);
+
+	 
+	Game.car = $('#my_car');
+	Game.enemy = $('#enemy_car');
+
+	$(document).bind('keydown', 'right', function() {
+		Game.moveRight();		
+	});
+	$(document).bind('keydown', 'left', function() {
+		Game.moveLeft();
+	});
+
+	Game.init();
 });
