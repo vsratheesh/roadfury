5698bb06a89e41700785a74dfd470a9459d2aa39
vsratheesh <vsratheesh95@gmail.com>
2016-03-04 16:17:18 +0530

    repeat of enemy

--- js/game.js
+++ js/game.js
@@ -1,8 +1,13 @@
-$(document).ready(function(){
-	var position = 0;
-	setInterval(function(){
-		position +=1;
-		var enemy = $('#enemy_car');
-		enemy.css("top",position+"vmin");
-	},30);
+$(document).ready(function() {
+  var position = -20;
+  setInterval(function(){
+    var enemy = $('#enemy_car');
+    if (position > 100) {
+      position = -20;
+      var left = Math.floor(Math.random()*5) * 10;
+      enemy.css("left", left + "vmin");
+    }
+    position += 1;
+    enemy.css("top", position + "vmin");
+  }, 30);
 });
